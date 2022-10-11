704e27ef482be56db495da5725722544e5b3ee46
Ravi Suhag <suhag.ravi@gmail.com>
2022-10-10 21:57:32 -0500

    feat: add firehose list command

--- cli/firehose/list.go
+++ cli/firehose/list.go
@@ -2,19 +2,57 @@ package firehose
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/odpf/salt/printer"
+	"github.com/odpf/salt/term"
 	"github.com/spf13/cobra"
+
+	"github.com/odpf/dex/generated/client/operations"
 )
 
 func listCommand() *cobra.Command {
+	var limit int
+
 	cmd := &cobra.Command{
 		Use:   "list",
-		Short: "List a firehose",
+		Short: "List firehoses",
 	}
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Creating firehose...")
+		spinner := printer.Spin("")
+		defer spinner.Stop()
+
+		client := initClient()
+
+		params := operations.ListFirehosesParams{}
+		res, err := client.Operations.ListFirehoses(&params)
+		if err != nil {
+			return err
+		}
+
+		firehoses := res.Payload.Items
+
+		report := [][]string{}
+
+		index := 1
+		report = append(report, []string{
+			term.Bold("INDEX"),
+			term.Bold("NAME"),
+		})
+		for _, f := range firehoses {
+			report = append(report, []string{term.Greenf("#%d", index), f.Name})
+			index++
+		}
+
+		spinner.Stop()
+		fmt.Printf("\nShowing %d firehoses", len(firehoses))
+		printer.Table(os.Stdout, report)
+
 		return nil
 	}
+
+	cmd.Flags().IntVarP(&limit, "limit", "L", 30, "Maximum number of firehoses to fetch (default 30)")
+
 	return cmd
 }
--- cli/firehose/view.go
+++ cli/firehose/view.go
@@ -1,6 +1,7 @@
 package firehose
 
 import (
+	"github.com/odpf/salt/printer"
 	"github.com/spf13/cobra"
 )
 
@@ -8,9 +9,13 @@ func viewCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "view",
 		Short: "View a firehose",
+		Long:  "Display information about a firehose",
 	}
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		spinner := printer.Spin("")
+		defer spinner.Stop()
+
 		return nil
 	}
 	return cmd
