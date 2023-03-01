f90c171d0baa4bfa6444ce708711cce07f6ea270
Shivaprasad <shiv.ylp@gmail.com>
2023-03-01 12:59:56 +0530

    fix: sink_type check

--- internal/server/v1/firehose/mappings.go
+++ internal/server/v1/firehose/mappings.go
@@ -110,12 +110,24 @@ func makeLabelsMap(def models.Firehose) map[string]string {
 }
 
 func makeConfigStruct(cfg *models.FirehoseConfig, prj *shieldv1beta1.Project) (*structpb.Value, error) {
-	if cfg.BootstrapServers == nil {
+	switch {
+	case cfg.BootstrapServers == nil:
 		return nil, errors.ErrInvalid.WithMsgf("bootstrap_servers must be set")
-	} else if cfg.TopicName == nil {
+
+	case cfg.TopicName == nil:
 		return nil, errors.ErrInvalid.WithMsgf("topic_name must be set")
-	} else if cfg.ConsumerGroupID == nil {
+
+	case cfg.ConsumerGroupID == nil:
 		return nil, errors.ErrInvalid.WithMsgf("consumer_group_id must be set")
+
+	case cfg.SinkType == nil:
+		return nil, errors.ErrInvalid.WithMsgf("sink_type must be set")
+
+	case cfg.StreamName == nil:
+		return nil, errors.ErrInvalid.WithMsgf("stream_name must be set")
+
+	case cfg.InputSchemaProtoClass == nil:
+		return nil, errors.ErrInvalid.WithMsgf("input_schema_proto_class must be set")
 	}
 
 	var stopAt *time.Time
@@ -142,6 +154,10 @@ func makeConfigStruct(cfg *models.FirehoseConfig, prj *shieldv1beta1.Project) (*
 			telegrafConf = map[string]interface{}{"enabled": false}
 		}
 	}
+
+	cfg.EnvVars["SINK_TYPE"] = string(*cfg.SinkType)
+	cfg.EnvVars["STREAM_NAME"] = *cfg.StreamName
+	cfg.EnvVars["INPUT_SCHEMA_PROTO_CLASS"] = *cfg.InputSchemaProtoClass
 
 	return utils.GoValToProtoStruct(moduleConfig{
 		State:    "RUNNING",
